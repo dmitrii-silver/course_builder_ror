5de8ce066c3a4006b4f53134167c0ac8151fbf26
dmitrii-silver <dmitrii.serebriakov@hotmail.com>
2022-05-23 11:13:11 +0100

    Fixed course model and testes

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,8 +1,9 @@
 class Course < ApplicationRecord
   has_many :unit_courses
   has_many :units, through: :unit_courses
-  # validates :name, presence: true
-  # validates :volume, presence: true
-  # validates :annotation, presence: true
-  # validates :description_text, presence: true
+  
+  validates :name, presence: true
+  validates :volume, presence: true
+  validates :annotation, presence: true
+  validates :description_text, presence: true
 end
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -2,28 +2,49 @@ require "rails_helper"
 
 RSpec.describe Course, type: :model do
   context "validations tests" do
-    it "ensures the title is present" do
-      course = Course.new(body: "Content of the body")
-      expect(course.valid?).to eq(false)
+    it "ensures the name is present" do
+      course = Course.new(volume: "Content of the volume")
+      expect(course.name.present?).to eq(false)
     end
 
-    it "ensures the body is present" do
-      course = Course.new(title: "Title")
-      expect(course.valid?).to eq(false)
+    it "ensures that name matches" do
+      course = Course.new(name: "Name")
+      expect(course.name).to eq("Name")
     end
 
-    it "ensures the article is active by default" do
-      course = Course.new(body: "Content of the body", title: "Title")
-      expect(course.active?).to eq(true)
+    it "ensures the volume is present" do
+      course = Course.new(name: "Name")
+      expect(course.volume.present?).to eq(false)
     end
 
-    it "should be able to save article" do
-      course = Course.new(body: "Content of the body", title: "Title")
+    it "ensures that volume matches" do
+      course = Course.new(volume: "Content of the volume")
+      expect(course.volume).to eq("Content of the volume")
+    end       
+
+    it "ensures the description_text is present" do
+      course = Course.new(name: "Name")
+      expect(course.description_text.present?).to eq(false)
+    end
+
+    it "ensures that description_text matches" do
+      course = Course.new(description_text: "Description text")
+      expect(course.description_text).to eq("Description text")
+    end
+
+    it "ensures the annotation is present" do
+      course = Course.new(volume: "Content of the volume", name: "Name")
+      expect(course.annotation.present?).to eq(false)
+    end
+
+    it "ensures that annotation matches" do
+      course = Course.new(annotation: "Content of the annotation")
+      expect(course.annotation).to eq("Content of the annotation")
+    end    
+
+    it "should be able to save course" do
+      course = Course.new(name: "Name", volume: "Content of the body", annotation: "Annotation", description_text: "Description text")
       expect(course.save).to eq(true)
     end
-  end
-
-  context "scopes tests" do
-
   end
 end
