cd85c50ce328c958002f8152384115521992841d
dmitrii-silver <dmitrii.serebriakov@hotmail.com>
2022-05-29 13:01:44 +0100

    Added test for routes of course_controller.rb

--- spec/requests/courses_spec.rb
+++ spec/requests/courses_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "Courses", type: :request do
+RSpec.describe "Courses", type: :routing do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "routes /courses to the courses controller" do
+      expect(get("/courses")).to route_to("courses#index")
+    end
   end
 end
