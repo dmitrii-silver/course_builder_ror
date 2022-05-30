6188c26a9e58d519becefb91833ba6683d5f0ea2
dmitrii-silver <dmitrii.serebriakov@hotmail.com>
2022-05-30 08:51:45 +0100

    Added views index, show and new to courses. Created function (new) for course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,23 +1,35 @@
 class CoursesController < ApplicationController
   def index
-  	@courses=Course.all
+    @courses=Course.all
   end
 
   def new
+    @course = Course.new
   end
 
   def edit
   end
 
   def show
+    @course = Course.find(params[:id])
   end
 
   def create
+    @course = Course.create(course_params)
+
+    redirect_to course_path(@course)
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def course_params    
+    params.require(:course).permit(:name, :volume, :annotation,
+                               :description_text)
   end
 end
