bfb2618bf66169a56904b7fd4ac207c00ea39bbc
dmitrii-silver <dmitrii.serebriakov@hotmail.com>
2022-06-06 12:31:02 +0100

    Added tests for courses_controller new action

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -18,17 +18,38 @@ RSpec.describe CoursesController, type: :controller do
   describe "create action" do
     context "GET #create" do
       it "redirects to show" do
-        post :create, :params => { :course => { name: "Course", volume: "Volume", annotation: "Annotation", description_text: "Text" } }
+        post :create, params: {
+          course: {
+            name: "Course",
+            volume: "Volume",
+            annotation: "Annotation",
+            description_text: "Text" 
+            } 
+          }
         expect(response).to redirect_to(course_path(assigns(:course)))
       end
 
       it "renders new page" do
-        post :create, :params => { :course => { name: nil, volume: nil, annotation: "Annotation", description_text: "Text" } }
+        post :create, params: {
+          course: {
+           name: nil,
+           volume: nil,
+           annotation: "Annotation",
+           description_text: "Text"
+          }
+        }
         expect(response).to render_template("new")
       end
 
       it "responds to html by default" do
-        post :create, :params => { :course => { name: "Course", volume: "Volume", annotation: "Annotation", description_text: "Text" } }
+        post :create, params: {
+          course: {
+            name: "Course",
+            volume: "Volume",
+            annotation: "Annotation",
+            description_text: "Text"
+          }
+        }
         expect(response.content_type).to eq "text/html; charset=utf-8"
       end
     end
@@ -36,10 +57,50 @@ RSpec.describe CoursesController, type: :controller do
   
   describe "show action" do
     context "GET #show" do
-    let(:course) { Course.create(name: "Course", volume: "Volume", annotation: "Annotation", description_text: "Text") }
+    let(:course) { Course.create(
+      name: "Course",
+      volume: "Volume",
+      annotation: "Annotation",
+      description_text: "Text"
+      )
+    }
       it "return a success response" do
         get :show, params: { id: course }
         expect(response.status).to eq(200)
+      end
+    end
+  end
+
+  describe "new action" do
+    context "GET #new" do
+      it "has a 200 status code" do
+        get :new
+        expect(response.status).to eq(200)
+      end
+
+      it "renders the new template" do
+        get :new
+        expect(response).to render_template("new")
+      end
+
+      it "is valid with complete data" do
+        course = Course.new(
+          name: "Course",
+          volume: "Volume",
+          annotation: "Annotation",
+          description_text: "Text"
+          )
+        expect(course.valid?).to be true
+      end
+      
+      it "is invalid with incomplete data" do
+        course = Course.new(
+          name: "Course",
+          volume: nil,
+          annotation: "Annotation",
+          description_text: "Text"
+          )
+        expect(course.valid?).to be false
       end
     end
   end
